35bc8186f28efe45b85c3324752901b2b80b937f
dong-jinliang <dong-jinliang@meyougames.com>
2013-04-10 14:18:54 +0800

    s2

---
+++ ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  test11AppController.h
+//  test11
+//
+//  Created by MeYouGamesP5 on 13-4-10.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  test11AppController.h
+//  test11
+//
+//  Created by MeYouGamesP5 on 13-4-10.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
